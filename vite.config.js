48aad43bb8961097cb4dafd4caf8e88cecef8358
naouel89 <jessus.n@gmail.COM>
2023-07-18 11:02:48 +0200

    configV

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import{defineConfig} from 'vite'
-<<<<<<< HEAD
 
 export default defineConfig({
   build: {
-    rolluppOptions: {
+    rollupOptions: {
       output: {
         entryFileNames: `assets/[name].js`,
         chunkFileNames: `assets/[name].js`,
@@ -12,19 +11,4 @@ export default defineConfig({
     }
   }
     })
-=======
-export default defineConfig(({ command, mode, ssrBuild }) => {
-  return {
 
-    build: {
-      rollupOptions: {
-        output: {
-          entryFileNames: `assets/[name].js`,
-          chunkFileNames: `assets/[name].js`,
-          assetFileNames: `assets/[name].[ext]`
-        }
-      }
-    }
-  }
-})
->>>>>>> 73e197d5e8bc226b2076977c798640e71c423506
