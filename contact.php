71802f6d6fcea4f7f538c50025062e7718368a17
naouel89 <jessus.n@gmail.COM>
2023-07-19 14:44:02 +0200

    C

--- contact.php
+++ contact.php
@@ -1,26 +1,10 @@
-<!doctype html>
-<html lang="fr">
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>The District: -Contact</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-  
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-    integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="dist/assets/index-cf548c1d.css">
-
-</head>
-
-<body>
-
-<?php include 'header.php';
+<?php 
+$titre = "contact";
+include 'header.php';
+include 'navbar.php';
 ?>
 
-      
       
 
   <!--faire la page Contact-->
