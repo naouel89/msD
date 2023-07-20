33b8f30d2262c048b6c70e5e2e7951df13bb8f48
naouel89 <jessus.n@gmail.COM>
2023-07-20 16:24:12 +0200

    oklm

--- nosburger.php
+++ nosburger.php
@@ -13,42 +13,42 @@ include 'navbar.php';
     <div class="col-md-4">
       <div class="category">
         <a class="burger-link" href="#" data-burger="CheeseBurgers">
-          <img src="images_the_district(1)/images_the_district/burger.jpg" alt="Nos Burger">
-        </a>
+          <img src="images_the_district(1)/images_the_district/burger.jpg" alt="Nos Burger" class="effet">
+        </a></div>
         <h3>CheeseBurgers</h3>
         <p>Prix: 5.99 €</p>
         <p>Description: Un délicieux burger garni de fromage fondant.</p>
         <button class="btn btn-primary" onclick="addToCart('CheeseBurgers')">Ajouter</button>
         <input type="number" id="quantity-CheeseBurgers" min="1" value="1">
       </div>
-    </div>
+    
 
     <!-- Burger 2 - DoubleBurgers -->
     <div class="col-md-4">
       <div class="category">
         <a class="burger-link" href="#" data-burger="DoubleBurgers">
-          <img src="images_the_district(1)/images_the_district/menu-burger.jpg" alt="Nos Burger">
-        </a>
+          <img src="images_the_district(1)/images_the_district/menu-burger.jpg" alt="Nos Burger" class="effet">
+        </a></div>
         <h3>DoubleBurgers</h3>
         <p>Prix: 7.99 €</p>
         <p>Description: Un burger double garni de viande savoureuse.</p>
         <button class="btn btn-primary" onclick="addToCart('DoubleBurgers')">Ajouter</button>
         <input type="number" id="quantity-DoubleBurgers" min="1" value="1">
       </div>
-    </div>
+    
 
     <!-- Burger 3 - Burgers 3 -->
     <div class="col-md-4">
       <div class="category">
         <a class="burger-link" href="#" data-burger="Burgers3">  
-          <img src="images_the_district(1)/images_the_district/food/cheesburger.jpg" alt="Nos Burger">
-        </a>
+          <img src="images_the_district(1)/images_the_district/food/cheesburger.jpg" alt="Nos Burger" class="effet">
+        </a></div>
         <h3>Burgers 3</h3>
         <p>Prix: 6.49 €</p>
         <p>Description: Un trio de burgers aux saveurs uniques.</p>
         <button class="btn btn-primary" onclick="addToCart('Burgers3')">Ajouter</button>
         <input type="number" id="quantity-Burgers3" min="1" value="1">
-      </div>
+      
     </div>
   </div>
 
@@ -57,36 +57,36 @@ include 'navbar.php';
     <div class="col-md-4">
       <div class="category">
         <a class="burger-link" href="#" data-burger="DoubleSteakBurgers">
-          <img src="images_the_district(1)/images_the_district/food/Food-Name-433.jpeg" alt="Nos Burger">
-        </a>
+          <img src="images_the_district(1)/images_the_district/food/Food-Name-433.jpeg" alt="Nos Burger" class="effet">
+        </a></div>
         <h3>Double Steak Burgers</h3>
         <p>Prix: 8.99 €</p>
         <p>Description: Deux steaks juteux dans un seul burger.</p>
         <button class="btn btn-primary" onclick="addToCart('DoubleSteakBurgers')">Ajouter</button>
         <input type="number" id="quantity-DoubleSteakBurgers" min="1" value="1">
       </div>
-    </div>
+    
 
     <!-- Burger 5 - Geante Burgers -->
     <div class="col-md-4">
       <div class="category">
         <a class="burger-link" href="#" data-burger="GeanteBurgers">
-          <img src="images_the_district(1)/images_the_district/food/hamburger.jpg" alt="Nos Burger">
-        </a>
+          <img src="images_the_district(1)/images_the_district/food/hamburger.jpg" alt="Nos Burger" class="effet">
+        </a></div>
         <h3>Geante Burgers</h3>
         <p>Prix: 9.99 €</p>
         <p>Description: Un burger géant pour les plus gros appétits.</p>
         <button class="btn btn-primary" onclick="addToCart('GeanteBurgers')">Ajouter</button>
         <input type="number" id="quantity-GeanteBurgers" min="1" value="1">
-      </div>
+      
     </div>
 
     <!-- Burger 6 - Burgers 6 -->
     <div class="col-md-4">
       <div class="category">
         <a class="burger-link" href="#" data-burger="Burgers6">
-          <img src="images_the_district(1)/images_the_district/food/Food-Name-6340.jpg" alt="Nos Burger">
-        </a>
+          <img src="images_the_district(1)/images_the_district/food/Food-Name-6340.jpg" alt="Nos Burger" >
+        </a></div>
         <h3>Burgers 6</h3>
         <p>Prix: 7.49 €</p>
         <p>Description: Six mini burgers aux goûts variés.</p>
@@ -94,7 +94,7 @@ include 'navbar.php';
         <input type="number" id="quantity-Burgers6" min="1" value="1">
       </div>
     </div>
-  </div>
+  
 </div>
 
 <div class="col-md">
