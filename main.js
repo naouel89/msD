2a619186f9f8f8d7d8d3b8bf6a74ce9cee75f635
naouel89 <jessus.n@gmail.COM>
2023-07-24 11:27:28 +0200

    12

--- main.js
+++ main.js
@@ -41,49 +41,51 @@ function validateForm(event) {
 
     // panier
 
-    function addToCart(burgerName, price) {
-      const quantity = parseInt(document.querySelector(`.burger-name=${burgerName} .quantity input`).value);
-      const total = price * quantity;
-      const cartItems = document.getElementById('cart-items');
-      const cartTotal = document.getElementById('cart-total');
+    let cartItems = [];
 
-      const itemText = `${quantity}x ${burgerName} - ${total.toFixed(2)} €`;
-      const item = document.createElement('li');
-      item.innerText = itemText;
-      cartItems.appendChild(item);
-
-      let currentTotal = parseFloat(cartTotal.innerText.replace(/[^0-9.]/g, ''));
-      currentTotal += total;
-      cartTotal.innerText = `Total: ${currentTotal.toFixed(2)} €`;
-
-      // Ajouter l'article au résumé de la commande
-      const orderItems = document.getElementById('order-items');
-      const orderTotal = document.getElementById('order-total');
-      const orderItem = document.createElement('li');
-      orderItem.innerText = itemText;
-      orderItems.appendChild(orderItem);
-
-      let currentOrderTotal = parseFloat(orderTotal.innerText.replace(/[^0-9.]/g, ''));
-      currentOrderTotal += total;
-      orderTotal.innerText = `Total: ${currentOrderTotal.toFixed(2)} €`;
+    function addToCart(burgerName, burgerPrice) {
+    const quantity = parseFloat(document.getElementById('quantity-' + burgerName).value);
+    const totalPrice = burgerPrice * quantity;
+    const cartItem = { name: burgerName, price: totalPrice };
+    cartItems.push(cartItem);
+    updateCartUI();
     }
-
+    
+    function updateCartUI() {
+    const cartItemsList = document.getElementById('cart-items');
+    const cartTotal = document.getElementById('cart-total');
+    let cartItemsHTML = '';
+    let cartTotalAmount = 0;
+    
+    for (const item of cartItems) {
+    cartItemsHTML += `<li>${item.name} - ${item.price.toFixed(2)} €</li>`;
+    cartTotalAmount += item.price;
+    }
+    
+    cartItemsList.innerHTML = cartItemsHTML;
+    cartTotal.innerText = `Total: ${cartTotalAmount.toFixed(2)} €`;
+    document.getElementById('cart-items-input').value = JSON.stringify(cartItems);
+    document.getElementById('total-price-input').value = cartTotalAmount.toFixed(2);
+    
+    updateOrderSummaryUI();
+    }
+    
+    function updateOrderSummaryUI() {
+    const orderItemsList = document.getElementById('order-items');
+    const orderTotal = document.getElementById('order-total');
+    let orderItemsHTML = '';
+    let orderTotalAmount = 0;
+    
+    for (const item of cartItems) {
+    orderItemsHTML += `<li>${item.name} - ${item.price.toFixed(2)} €</li>`;
+    orderTotalAmount += item.price;
+    }
+    
+    orderItemsList.innerHTML = orderItemsHTML;
+    orderTotal.innerText = `Total: ${orderTotalAmount.toFixed(2)} €`;
+    }
+    
     function clearCart() {
-      const cartItems = document.getElementById('cart-items');
-      const cartTotal = document.getElementById('cart-total');
-      cartItems.innerHTML = '';
-      cartTotal.innerText = 'Total: 0.00 €';
-
-      // Vider également le résumé de la commande
-      const orderItems = document.getElementById('order-items');
-      const orderTotal = document.getElementById('order-total');
-      orderItems.innerHTML = '';
-      orderTotal.innerText = 'Total: 0.00 €';
-    }
-
-    function checkout() {
-      const cartItems = document.getElementById('cart-items').innerHTML;
-      const cartTotal = document.getElementById('cart-total').innerText;
-      document.getElementById('cart-items-input').value = cartItems;
-      document.getElementById('total-price-input').value = cartTotal;
+    cartItems = [];
+    updateCartUI();
     }
---
+++ menu_items.php
@@ -0,0 +1,73 @@
+<!-- Burger 1 - CheeseBurgers -->
+<div class="col-md-4">
+  <div class="category">
+    
+      <img src="images_the_district(1)/images_the_district/burger.jpg" alt="CheeseBurgers" class="effet">
+    </a>
+  </div>
+  <h3>CheeseBurgers</h3>
+  <p>Prix: 5.99 €</p>
+  <p>Description: Un délicieux burger garni de fromage fondant.</p>
+  <button class="btn btn-primary" onclick="addToCart('CheeseBurgers', 5.99)">Ajouter</button>
+  <input type="number" id="quantity-CheeseBurgers" min="1" value="1">
+</div>
+
+<!-- Burger 2 - DoubleBurgers -->
+<div class="col-md-4">
+  <!-- ... Other menu items ... -->
+</div>
+
+<!-- ... More menu items ... -->
+<script>
+// on essai
+
+
+let cartItems = [];
+
+function addToCart(burgerName, burgerPrice) {
+const quantity = parseFloat(document.getElementById('quantity-' + burgerName).value);
+const totalPrice = burgerPrice * quantity;
+const cartItem = { name: burgerName, price: totalPrice };
+cartItems.push(cartItem);
+updateCartUI();
+}
+
+function updateCartUI() {
+const cartItemsList = document.getElementById('cart-items');
+const cartTotal = document.getElementById('cart-total');
+let cartItemsHTML = '';
+let cartTotalAmount = 0;
+
+for (const item of cartItems) {
+cartItemsHTML += `<li>${item.name} - ${item.price.toFixed(2)} €</li>`;
+cartTotalAmount += item.price;
+}
+
+cartItemsList.innerHTML = cartItemsHTML;
+cartTotal.innerText = `Total: ${cartTotalAmount.toFixed(2)} €`;
+document.getElementById('cart-items-input').value = JSON.stringify(cartItems);
+document.getElementById('total-price-input').value = cartTotalAmount.toFixed(2);
+
+updateOrderSummaryUI();
+}
+
+function updateOrderSummaryUI() {
+const orderItemsList = document.getElementById('order-items');
+const orderTotal = document.getElementById('order-total');
+let orderItemsHTML = '';
+let orderTotalAmount = 0;
+
+for (const item of cartItems) {
+orderItemsHTML += `<li>${item.name} - ${item.price.toFixed(2)} €</li>`;
+orderTotalAmount += item.price;
+}
+
+orderItemsList.innerHTML = orderItemsHTML;
+orderTotal.innerText = `Total: ${orderTotalAmount.toFixed(2)} €`;
+}
+
+function clearCart() {
+cartItems = [];
+updateCartUI();
+}
+</script>
